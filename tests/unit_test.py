4775147f14b77c252339a3ad91e8c32d0550a6ab
Sadik Bakiu <sbakiu@gmail.com>
2022-07-31 17:01:01 +0200

    Handle pytest command better

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,2 +1,11 @@
+from src.app import get_string
+
+
 def test_dummy():
     assert 1 == 1
+
+
+def test_get_string():
+    expected_string = "string"
+    response = get_string()
+    assert response == expected_string
